1384c705bfe0e4b8638557abd59630b925267b76
Pob <pob@zipmex.com>
2021-11-30 02:27:37 +0700

    Add picture & css/less style into the publishable lib

--- libs/button-publish/src/lib/button-publish.tsx
+++ libs/button-publish/src/lib/button-publish.tsx
@@ -1,5 +1,8 @@
 import { Button } from 'antd';
 import './button-publish.module.less';
+import picSrc from './pic.png';
+import styles from './style.module.css';
+import stylesLess from './styleLess.module.less';
 
 /* eslint-disable-next-line */
 export interface ButtonPublishProps {}
@@ -7,7 +10,11 @@ export interface ButtonPublishProps {}
 export function MyButtonPublish(props: ButtonPublishProps) {
   return (
     <div>
-      <div>Button-Publish:</div>
+      <div>
+        <img src={picSrc} alt="" />{' '}
+        <span className={stylesLess.pob}>Button</span>-
+        <span className={styles.pob}>Publish</span>:
+      </div>
       <Button type="primary">Btn-Pub!!</Button>
     </div>
   );
